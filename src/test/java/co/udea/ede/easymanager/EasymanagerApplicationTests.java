023f63685267a646eb13bec4390328cc1d4b26f6
Elverarroyave <elver.arroyave@gmail.com>
2020-11-24 09:27:45 -0500

    Git init

---
+++ src/test/java/co/udea/ede/easymanager/EasymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.udea.ede.easymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
