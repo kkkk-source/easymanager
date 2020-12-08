0a9bfbc64472aaa6bc13bdf1fb43721976e051cc
k <mol.tired@gmail.com>
2020-12-07 23:35:10 -0500

    add sale model

---
+++ src/main/java/co/edu/udea/easymanager/sale/model/SaleId.java
@@ -0,0 +1,20 @@
+package co.edu.udea.easymanager.sale.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Generated
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleId implements Serializable {
+
+    private Long id;
+
+    private Long product;
+
+}
