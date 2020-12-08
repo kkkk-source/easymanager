18c44c0d48bfe0336da7ef5f3df55481960802dc
k <mol.tired@gmail.com>
2020-12-08 11:23:30 -0500

    add sale io

---
+++ src/main/java/co/edu/udea/easymanager/sale/io/gateway/SaleGatewayImpl.java
@@ -0,0 +1,56 @@
+package co.edu.udea.easymanager.sale.io.gateway;
+
+import co.edu.udea.easymanager.shared.web.exception.ResourceNotFoundException;
+import co.edu.udea.easymanager.sale.io.repository.SaleRepository;
+import co.edu.udea.easymanager.sale.model.Sale;
+import co.edu.udea.easymanager.sale.model.SaleId;
+import co.edu.udea.easymanager.sale.service.SaleGateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.Predicate;
+import javax.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Repository
+class SaleGatewayImpl implements SaleGateway {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final String RESOURCE_NOT_FOUND = "Sale not found";
+    private SaleRepository saleRepository;
+
+    public SaleGatewayImpl(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    }
+
+    @Override
+    public Sale save(@NotNull Sale saleToCreate) {
+        logger.debug("Begin save saleToCreate = {}", saleToCreate);
+        final Sale saleToBeCreated = saleToCreate.toBuilder()
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
+        final Sale saleCreated = saleRepository.save(saleToBeCreated);
+        logger.debug("End save saleCreated = {}", saleCreated);
+        return saleCreated;
+    }
+
+    @Override
+    public List<Sale> findBySaleId(@NotNull Long id) {
+        logger.debug("Begin findById id = {}", id);
+        List<Sale> salesFound = saleRepository.findById(id);
+        logger.debug("End findById salesFound = {}", salesFound);
+        return salesFound;
+    }
+}
---
+++ src/main/java/co/edu/udea/easymanager/sale/io/web/v1/model/SaleSaveResponse.java
@@ -0,0 +1,37 @@
+package co.edu.udea.easymanager.sale.io.web.v1.model;
+
+import co.edu.udea.easymanager.sale.model.Sale;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Generated
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleSaveResponse {
+
+    private Long id;
+
+    private Long product;
+
+    private Integer amount;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    public static SaleSaveResponse fromModel(Sale sale) {
+        return SaleSaveResponse.builder().id(sale.getId())
+                .product(sale.getProduct()).amount(sale.getAmount())
+                .createdDate(sale.getCreatedDate())
+                .updatedDate(sale.getUpdatedDate())
+                .build();
+    }
+}
